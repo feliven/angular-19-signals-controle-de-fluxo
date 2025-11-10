397e3140bfba8b37a7ea68c7f054fbf128b5294b
FVS <feliven@gmail.com>
2025-11-10 14:37:12 -0300

    criado mock-livros

---
+++ src/app/mock-livros.ts
@@ -0,0 +1,39 @@
+import { Livro } from './shared/livro.interface';
+
+export const listaLivros: Livro[] = [
+  {
+    titulo: 'E não sobrou nenhum',
+    autoria: 'Agatha Christie',
+    favorito: false,
+    genero: 'Literatura',
+    imagem: 'https://placehold.co/128x185',
+  },
+  {
+    titulo: 'Canções',
+    autoria: 'Cecília Meireles',
+    favorito: false,
+    genero: 'Poesia',
+    imagem: 'https://placehold.co/128x185',
+  },
+  {
+    titulo: 'Rita Lee: Outra autobiografia',
+    autoria: 'Rita Lee',
+    favorito: false,
+    genero: 'Biografias',
+    imagem: 'https://placehold.co/128x185',
+  },
+  {
+    titulo: 'Abecedário do Folclore Brasileiro',
+    autoria: 'Januária Cristina Alves',
+    favorito: false,
+    genero: 'Mitos e Lendas',
+    imagem: 'https://placehold.co/128x185',
+  },
+  {
+    titulo: 'Estruturas de dados e algoritsmo com JavaScript',
+    autoria: 'Loiane Groner',
+    favorito: false,
+    genero: 'Técnicos',
+    imagem: 'https://placehold.co/128x185',
+  },
+];
