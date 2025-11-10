f4ed7bd0a0091f7a77bb29e5800b5abb8b4724cf
FVS <feliven@gmail.com>
2025-11-10 12:42:37 -0300

    ajustes pequenos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
-import { ListaCategoriaComponent } from './components/lista-categoria/lista-categoria.component';
 import { MinhaOrganizacaoComponent } from './components/minha-organizacao/minha-organizacao.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    CabecalhoComponent,
-    RodapeComponent,
-    ListaCategoriaComponent,
-    MinhaOrganizacaoComponent,
-  ],
+  imports: [CabecalhoComponent, RodapeComponent, MinhaOrganizacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
