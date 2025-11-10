45117dd080b2d1ce6269ea41d2182dce86afc31f
FVS <feliven@gmail.com>
2025-11-10 11:19:45 -0300

    criados componentes cabecalho, rodape, botao, card e lista-categoria

---
+++ src/app/components/lista-categoria/lista-categoria.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-categoria',
+  imports: [],
+  templateUrl: './lista-categoria.component.html',
+  styleUrl: './lista-categoria.component.css'
+})
+export class ListaCategoriaComponent {
+
+}
