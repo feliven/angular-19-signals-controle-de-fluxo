d21ade2b92b4c1663129c89336c573ff2ae669ce
FVS <feliven@gmail.com>
2025-11-11 10:02:56 -0300

    usa ngOnInit e forEach para reunir livros por categoria

--- src/app/components/lista-categoria/lista-categoria.component.ts
+++ src/app/components/lista-categoria/lista-categoria.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { listaLivros } from '../../mock-livros';
+import { Livro } from '../../shared/livro.interface';
+import { CategoriaELivro } from '../../shared/categoria-e-livro.interface';
 
 @Component({
   selector: 'app-lista-categoria',
@@ -8,8 +10,8 @@ import { listaLivros } from '../../mock-livros';
   templateUrl: './lista-categoria.component.html',
   styleUrl: './lista-categoria.component.css',
 })
-export class ListaCategoriaComponent {
-  categorias = [
+export class ListaCategoriaComponent implements OnInit {
+  categorias2 = [
     'Literatura',
     'Poesia',
     'Biografias',
@@ -18,8 +20,45 @@ export class ListaCategoriaComponent {
   ];
 
   livros = listaLivros;
+  livrosPorCategoria: Map<string, Livro[]> = new Map();
+
+  ngOnInit() {
+    this.livrosPorCategoria = new Map();
+    const livros = listaLivros;
+
+    livros.forEach((livro) => {
+      const categoria = livro.categoria;
+      if (!this.livrosPorCategoria.has(categoria)) {
+        this.livrosPorCategoria.set(categoria, []);
+      }
+      this.livrosPorCategoria.get(categoria)?.push(livro);
+    });
+  }
+
+  categorias: CategoriaELivro[] = [
+    {
+      nome: 'Literatura',
+      livros: this.livrosPorCategoria.get('Literatura') ?? [],
+    },
+    {
+      nome: 'Poesia',
+      livros: this.livrosPorCategoria.get('Poesia') ?? [],
+    },
+    {
+      nome: 'Biografias',
+      livros: this.livrosPorCategoria.get('Biografias') ?? [],
+    },
+    {
+      nome: 'Mitos e Lendas',
+      livros: this.livrosPorCategoria.get('Mitos e Lendas') ?? [],
+    },
+    {
+      nome: 'Técnicos',
+      livros: this.livrosPorCategoria.get('Técnicos') ?? [],
+    },
+  ];
 
   obterLivrosPorCategoria(categoria: string) {
-    return this.livros.filter((livro) => livro.genero === categoria);
+    return this.livros.filter((livro) => livro.categoria === categoria);
   }
 }
